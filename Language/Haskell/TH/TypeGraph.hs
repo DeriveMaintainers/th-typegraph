146dc16d93175f85fbcd462645a426fdd60676b1
David Fox <dsf@seereason.com>
2015-05-07 21:43:00 -0700

    Add a re-export module

---
+++ Language/Haskell/TH/TypeGraph.hs
@@ -0,0 +1,21 @@
+module Language.Haskell.TH.TypeGraph
+    ( module Language.Haskell.TH.TypeGraph.Core
+    , module Language.Haskell.TH.TypeGraph.Edges
+    , module Language.Haskell.TH.TypeGraph.Expand
+    , module Language.Haskell.TH.TypeGraph.Graph
+    , module Language.Haskell.TH.TypeGraph.Hints
+    , module Language.Haskell.TH.TypeGraph.Info
+    , module Language.Haskell.TH.TypeGraph.Monad
+    , module Language.Haskell.TH.TypeGraph.Unsafe
+    , module Language.Haskell.TH.TypeGraph.Vertex
+    ) where
+
+import Language.Haskell.TH.TypeGraph.Core
+import Language.Haskell.TH.TypeGraph.Edges
+import Language.Haskell.TH.TypeGraph.Expand
+import Language.Haskell.TH.TypeGraph.Graph
+import Language.Haskell.TH.TypeGraph.Hints
+import Language.Haskell.TH.TypeGraph.Info
+import Language.Haskell.TH.TypeGraph.Monad
+import Language.Haskell.TH.TypeGraph.Unsafe
+import Language.Haskell.TH.TypeGraph.Vertex
